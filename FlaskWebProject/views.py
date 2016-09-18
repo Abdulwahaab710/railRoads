c8893396e8af5fe71a7e7f69c28d6785158eb5cf
Abdulwahaab710 <Abdulwahaab.ahmed710@gmail.com>
2016-09-18 05:56:33 -0400

    bug fix

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template, Flask, request
-import db
+# import db
 from FlaskWebProject import app
 
 @app.route('/')
